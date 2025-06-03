4a7c43837e3389b2723bb2111fb68759a71d21f9
Phakkharaphong <phakkharaphong.c@kkumail.com>
2025-06-03 20:56:09 +0700

    update Project 1.add logic convert day and month 2. test Message position

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
+from datetime import datetime
 from flask import Flask,render_template,request,send_file
 from PIL import Image, ImageDraw, ImageFont # type: ignore
 import io
 
 #note
 #int is i, string is g, decimal falot is d  
-
 
 
 app = Flask(__name__)
@@ -27,7 +27,10 @@ def wallpaper():
     if request.method == 'POST':
         text = request.form['text']
         image_file = request.files['image']
-
+        position_x = request.form['position_x']
+        position_y = request.form["position_y"]
+        position_x = int(position_x)
+        position_y = int(position_y)
         # Open the image
         image = Image.open(image_file)
         draw = ImageDraw.Draw(image)
@@ -35,19 +38,18 @@ def wallpaper():
         # Choose a font (ensure the font file exists)
         font_path = "arial.ttf"  # Replace with the actual path to your font file
         try:
-            font = ImageFont.truetype(font_path, size=36)
+            font = ImageFont.truetype(font_path, size=70)
         except IOError:
             font = ImageFont.load_default()
 
         # Add text to the image
-        text_position = (50, 50)  # Adjust as needed
+        text_position = (position_x, position_y)  # Adjust as needed
         draw.text(text_position, text, font=font, fill=(255, 255, 255))  # White text
 
         # Save the modified image to a buffer
         img_buffer = io.BytesIO()
         image.save(img_buffer, format='PNG')
         img_buffer.seek(0)
-
         # Return the image as a response
         return send_file(img_buffer, mimetype='image/png')
     return render_template("/manage_wallpaper/wallpaper_manage.html")
@@ -63,9 +65,50 @@ def pform_wallpaper_brith_date():
         else:
             #accept requset from form to text
             text = request.form['select-day-form-dateofbirth']
-            print("Text is ",text)
+            date = request.form['converday']
+            month = request.form['convermonth']
+            year = request.form['year']
+            i_dayofnumber = g_case_of_day(date)
+            i__monthofnumber = g_case_of_month(month)
+            print("Text is ",text,"day is ",date,"month is ",month,"yearName is ",year ,"Day Of Number",
+                  i_dayofnumber, "Month of Number ",i__monthofnumber)
+
     return render_template("manage_wallpaper/wallpaper_manage_brithdate.html")
 
+
+#------------------ Funtion Method -------------------
+def g_case_of_day(_day):
+    switch={
+        'วันอาทิตย์': 1,
+        'วันจันทร์': 2,
+        'วันอังคาร': 3,
+        'วันพุธ': 4,
+        'วันพฤหัสบดี': 5,
+        'วันศุกร์': 6,
+        'วันเสาร์': 7
+    } 
+    return switch.get(_day,"not")
+
+def g_case_of_month(_month):
+    switch={
+        'มกราคม': 3,
+        'กุมภาพันธ์': 4,
+        'มีนาคม': 5,
+        'เมษายน': 6,
+        'พฤษภาคม': 7,
+        'มิถุนายน': 1,
+        'กรกฏาคม': 2,
+        'สิงหาคม': 3,
+        'กันยายน': 4,
+        'ตุลาคม': 5,
+        'พฤศจิกายน':1,
+        'ธันวาคม': 2
+
+    } 
+    return switch.get(_month,"not")
+
+
+#---------------------- End ----------------------
 
 if __name__ =="__main__":
     app.run(debug=True)
