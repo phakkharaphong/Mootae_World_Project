89f4f9c8e34ad037e9456567e930eb2bc6b94c55
FOLK <folkadpocket99@gmail.com>
2025-12-27 04:17:41 +0700

    change image behavior

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from 'next';
-import type { RemotePattern } from 'next/dist/shared/lib/image-config';
 
 const isProd = process.env.NODE_ENV === 'production';
 const basePath = isProd ? process.env.NEXT_PUBLIC_BASE_PATH || '' : '';
@@ -11,21 +10,7 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   basePath,
   images: {
-    remotePatterns: remoteImageUrls
-      .filter((u) => !!u)
-      .map((u): RemotePattern => {
-        const parsed = new URL(u.trim());
-        const protocol: 'http' | 'https' =
-          parsed.protocol === 'https:' ? 'https' : 'http';
-        const hostname = parsed.hostname;
-        const port = parsed.port ? parsed.port : undefined;
-        const basePath =
-          parsed.pathname && parsed.pathname !== '/' ? parsed.pathname : '/';
-        const pathname = basePath.endsWith('/')
-          ? `${basePath}**`
-          : `${basePath}/**`;
-        return { protocol, hostname, port, pathname };
-      }),
+    remotePatterns: [...remoteImageUrls.map((url) => new URL(url))],
   },
 };
 
--- src/app/(FOM)/Articles/page.tsx
+++ src/app/(FOM)/Articles/page.tsx
@@ -41,14 +41,12 @@ export default function ArticlesPage() {
         {data?.data.map((article) => (
           <article key={article.id} className="rounded-xl border p-4">
             <Image
-              src={
-                article.cover_img ||
-                withBasePath('/images/post-placeholder.webp')
-              }
+              src={article.cover_img || '/images/post-placeholder.webp'}
               className="mb-2 aspect-video w-full rounded-lg object-cover"
               alt={article.title}
               width={320}
               height={180}
+              unoptimized
             />
             <h3 className="mb-2 text-xl font-semibold">{article.title}</h3>
             <div className="text-end text-xs">
